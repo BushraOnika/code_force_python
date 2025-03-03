1718eff7250c01e007925e012e72c6ff79112e7b
Bushra Hossain <bushrahossainonika@gmail.com>
2025-03-03 14:29:50 +0600

    team

--- team.py
+++ team.py
@@ -1,11 +1,10 @@
 problems=int(input())
 total=0
 count=0
-for _ in range(problems):
-    for _ in range(3):
-        num=input().split(" ")
-        total+=int (num)
-    if (total>1):
+for i in range(problems):
+    a =list(map(int,input().split(" ")))
+    total+=a[i]
+    if(total>1):
         count+=1
 
 print(count)
