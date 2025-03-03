cd10f2dc53aed3199f8a1ea62403cd744fdfeb71
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-03-03 15:27:48 +0600

    Bit ++

--- f/bit++.py
+++ f/bit++.py
@@ -1,7 +1,27 @@
-n: int = int(input())
-x = 0
-for _ in range(n):
-    line: str = input()
-    center = line[len(line) // 2]
-    x += 1 if center == '+' else -1
-print(x)
+# https://codeforces.com/problemset/submission/282/308763114
+
+def bit_plus_plus(line: str) -> int:
+    """
+    :param line:
+    Operation ++
+    Operation --
+    :return:
+    "++"  ↦  1 | "--"  ↦  -1
+
+    >>> bit_plus_plus("X++")
+    1
+    >>> bit_plus_plus("--X")
+    -1
+    """
+    middle_number = len(line) // 2
+    middle_char = line[middle_number]
+    return 1 if middle_char == '+' else -1
+
+
+if __name__ == '__main__':
+    n: int = int(input())
+    x: int = 0
+    for _ in range(n):
+        line = input()
+        x += bit_plus_plus(line)
+    print(x)
